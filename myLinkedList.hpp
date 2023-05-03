42cf17d6316c9714565005161617b780079f30b3
C0MPU73R <55300593+C0MPU73R@users.noreply.github.com>
2023-05-03 17:12:12 -0400

    myLinkedList: Apply start of insert and deleting functionality

--- myLinkedList.hpp
+++ myLinkedList.hpp
@@ -7,7 +7,6 @@
 struct Node {
 public:
 	std::string data;
-private:
 	Node* prev = nullptr;
 	Node* next = nullptr;
 }
@@ -26,24 +25,39 @@ public:
 			node->data = data;
 			// this is the first node, hence it's prev and next pointers are still null. This is a doubley-linked list.
 			++this->size;
-			head = node; //redirect head to point towards this this new node. This node is now the head. It's
+			this->head = node; //redirect head to point towards this this new node. This node is now the head. It's
 			// tail is still pointing to null because we only have one node here.
 		}
 		else
 		{
-			//traverse down the LL datastructure, findthe end, then create and attach the new desired node.
-
+			//Head is not pointing to null hence it has to be pointing to its head node. account for this.
+			Node* node = new Node();
+			node->data = data;
+			node->prev = this->head;
 		}
 	}
 	void Delete(std::string data) {
+		modifier = head;
+		while (modifier != nullptr) {
+			if (modifier->data == data) {
 
+			}
+			else
+			{
+				modifier = modifier->next;
+			}
+		}
 	}
 	void printMyLinkedList() { // print the LL
+
+
+
 
 	}
 private:
 	Node* head = nullptr;
 	Node* tail = nullptr;
+	Node* modifier = nullptr;
 	int size = 0;
 	
 };
