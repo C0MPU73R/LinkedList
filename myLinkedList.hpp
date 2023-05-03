0a7af8dfc9a9678a55ce888744740c374638efd9
C0MPU73R <55300593+C0MPU73R@users.noreply.github.com>
2023-05-03 16:34:40 -0400

    myLinkedList: Start skeleton

---
+++ myLinkedList.hpp
@@ -0,0 +1,32 @@
+/*
+@filename: myLinkedList.hpp
+@author: C0MPU73R
+*/
+#include<string>
+
+struct Node {
+public:
+	std::string data;
+private:
+	Node* prev = nullptr;
+	Node* next = nullptr;
+}
+class myLinkedList {
+public:
+	myLinkedList() {}
+	myLinkedList(std::string data, Node* rhs) {
+		Node* node = new Node();
+		node->data = rhs->data;
+	}
+	void Insert(Node* node) {
+
+	}
+	void Delete(Node* node) {
+
+	}
+	void printMyLinkedList() { // print the LL
+
+	}
+private:
+	
+};
