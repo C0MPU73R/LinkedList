ba52d64815cfdd908843cda103fd82c8f0fec1bc
C0MPU73R <55300593+C0MPU73R@users.noreply.github.com>
2023-05-10 11:37:44 -0400

    Insert: Added more notes for elif implementation

--- myLinkedList/myLinkedList/myLinkedList.hpp
+++ myLinkedList/myLinkedList/myLinkedList.hpp
@@ -28,13 +28,17 @@ public:
 		Node* node = nullptr; // for initial creation
 		if (this->head == nullptr) { // explicitly check head first
 			node = new Node; // it is, so create a new node, this one will be head.
-			node->data = data;
+			node->data = data; // node is done being created. Go ahead and attach head to it (point head to it)
 			this->head = node; // now have a head
-			this -> tail = node; // now have a tail, as this is a single node in a single LL.
-			++this->size;
+			this -> tail = node; // now have a tail, as this is a single node in the single LL. The head and tail are the same at this point.
+			++this->size; // Increase  the size of the LL as we now have an element in the data structure.
 		}
 		else if ( this->head != nullptr && pos != this->size - 1) { // this is an insertion to somewhere in the LL that is not the beginning or the end.
-			
+			// else; if head is indeed not null, aka it is pointinng to something AND this is NOT the last node in the LL, go ahead and insert
+			//the new node here, keeping in mind that we will have to attach it's prev to the previous element and its next to the next element,
+			//if it exists that is. We then need to look at the nodes before and after this new node to adjust their own pointers so we do not
+			// 'short-circuit' this new node. Aka, we actually have a one-to-one list that is doubley linked, where there are no other strange loops 
+			//to other nodes in the LL.
 		}
 		else { // add it directly to the end of the list without doing an uneccessary traversal.
 			node = new Node;
